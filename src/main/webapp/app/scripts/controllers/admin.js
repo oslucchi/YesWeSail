872de5bb32f37616b12c65d812d21f51817eea2d
Stefanama <amarie.stefan@gmail.com>
2016-05-10 22:53:58 +0200

    Started working on admin page

---
+++ src/main/webapp/app/scripts/controllers/admin.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeswesailApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeswesailApp
+ */
+angular.module('yeswesailApp')
+  .controller('AdminCtrl', function ($scope, $http, URLs, MAPS, lodash ) {
+        
+       
+  });
