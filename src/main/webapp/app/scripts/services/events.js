d097c690b19f2bac82c60d991150d09a5a614875
stefanamarie <s.amarie@itsoftware.it>
2016-05-12 08:57:08 +0200

    continued work on create event page

---
+++ src/main/webapp/app/scripts/services/events.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name yeswesailApp.Events
+ * @description
+ * # Events
+ * Service in the yeswesailApp.
+ */
+angular.module('yeswesailApp')
+  .service('Events', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
