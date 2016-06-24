959a53994a030137b6bf0db7bf930ce204395839
osvaldo <o.lucchini@itsoftware.it>
2016-06-24 11:16:22 +0200

    added json for boats changed pom to resolve the mojo executions error

---
+++ src/main/java/com/yeswesail/rest/jsonInt/BoatsJson.java
@@ -0,0 +1,17 @@
+package com.yeswesail.rest.jsonInt;
+
+public class BoatsJson {
+	public int idBoats;
+	public int ownerId;
+	public String engineType;
+	public String plate;
+	public String name;
+	public String model;
+	public int length;
+	public int year;
+	public int cabinsWithBathroom;
+	public int cabinsNoBathroom;
+	public int sharedBathrooms;
+	public int bunks;
+
+}
