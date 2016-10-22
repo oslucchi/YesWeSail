797f0c15773169ea1a853f4370292ceee10e5c6f
Osvaldo Lucchini <osvaldo.lucchini@gmail.com>
2016-10-22 17:57:15 +0200

    - added dynamic pages handling

---
+++ src/main/java/com/yeswesail/rest/jsonInt/PagesJson.java
@@ -0,0 +1,11 @@
+package com.yeswesail.rest.jsonInt;
+
+import java.util.Date;
+
+public class PagesJson {
+	public Date createdOn;
+	public String uRLReference;
+	public String innerHTML;
+	public String language;
+	public String status;
+}
