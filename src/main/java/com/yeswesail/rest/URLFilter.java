5db848af4cdb91c3bad2bf4fc66e62e4771013f4
Osvaldo Lucchini <osvaldo.lucchini@gmail.com>
2017-03-02 23:55:08 +0100

    - added filter to handle rest call and angularjs endpoints - split package.properties in site and common

---
+++ src/main/java/com/yeswesail/rest/URLFilter.java
@@ -0,0 +1,56 @@
+package com.yeswesail.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+public class URLFilter implements Filter{
+	private final Logger log = (Logger) Logger.getLogger(getClass());
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException 
+	{
+		;
+	}
+
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) 
+			throws IOException, ServletException 
+	{
+		String path = ((HttpServletRequest) servletRequest).getServletPath();
+		if (path.startsWith("/rest"))
+		{
+			log.debug("trapped " + path + " call, forward to proper servlet");
+			servletRequest.getRequestDispatcher("/restcall" + path.substring(5))
+					.forward(servletRequest, servletResponse);
+			return;
+		}
+		else if (path.equals("/") || path.equals("/index.html") ||
+				 path.startsWith("/bower_components") || 
+				 path.startsWith("/images") || 
+				 path.startsWith("/resources") || 
+				 path.startsWith("/scripts") || 
+				 path.startsWith("/styles") || 
+				 path.startsWith("/views"))
+		{
+			chain.doFilter(servletRequest, servletResponse);
+			return;
+		}
+		
+		log.debug("request " + path + " must be handled. forwarding to index.html");
+		servletRequest.getRequestDispatcher("/index.html").forward(servletRequest, servletResponse);
+	}
+
+	@Override
+	public void destroy() 
+	{
+		;
+	}
+
+}
