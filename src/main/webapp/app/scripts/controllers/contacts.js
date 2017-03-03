d8c9d62a8ab84a402e71d2665e0ec5b26c58acb9
stefanamarie <s.amarie@itsoftware.it>
2017-03-03 11:51:21 +0100

    fixed various issues and started working on the contacts page

---
+++ src/main/webapp/app/scripts/controllers/contacts.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name yeswesailApp.controller:AdmineventsCtrl
+ * @description
+ * # AdmineventsCtrl
+ * Controller of the yeswesailApp
+ */
+angular.module('yeswesailApp').controller('ContactsCtrl', function ($scope, $http, URLs, MAPS, lodash, toastr, ngDialog, $filter, $translate) {
+   
+});
