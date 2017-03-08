86394734fba18f30040308d44daa3acc7925eca3
Osvaldo Lucchini <osvaldo.lucchini@gmail.com>
2017-03-08 01:27:17 +0100

    - added entries to be used while waiting for the external configuration file on the environment for app.js

--- src/main/webapp/app/scripts/app.js
+++ src/main/webapp/app/scripts/app.js
@@ -47,10 +47,10 @@ angular.module('yeswesailApp', [
     , SHIPOWNER: 6
     , ADMIN: 9
 }).constant('URLs', {
-     ddns: 'http://prod.yeswesail.com/'
-//	ddns: 'http://test.yeswesail.com/'
-    // ddns: 'http://lucchinis.ddns.com:8080/YesWeSail/'
-    // ddns: 'http://localhost:8080/YesWeSail/'
+	// ddns: 'http://prod.yeswesail.com/'
+	ddns: 'http://test.yeswesail.com/'
+	// ddns: 'http://localhost:8080/YesWeSail/'
+	// ddns: 'http://lucchinis.ddns.com'
 }).constant('LOCALES', {
     'locales': {
         'it_IT': 'Italiano',
